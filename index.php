e5a7ee93569eed0f1010f07225111a342c5aeecd
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2019-06-08 09:26:24 -0600

    index

--- index.php
+++ index.php
@@ -16,6 +16,9 @@
 	<script src="https://cdn.staticfile.org/jquery/2.1.1/jquery.min.js"></script>
 	<script src="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 	
 	
    <link rel="stylesheet" href="search.css">
@@ -185,7 +188,7 @@ body{
   <!--header for system heardermenu tooltip dropdown-->
     <h3 id="head1">Welcome to Travel Experts Database System (2019)</h3>
     <img src="icon.jpg" style="width:80px;margin-top:-10px">
-
+	<br><br>
 	<button style="float:right;margin-top:-20px;margin-right:10px" type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
       login
    </button>
