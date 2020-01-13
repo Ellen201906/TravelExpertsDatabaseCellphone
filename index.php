3b848bcf07dbba334ca8251ece69b910a10edae3
Ellen201906 <51275255+Ellen201906@users.noreply.github.com>
2020-01-13 09:51:38 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,23 +3,21 @@
 <head>
     <meta charset="utf-8" />
 	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	 <link rel="stylesheet" href="reset.css">
-    <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.1.0/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 	
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.7.0/css/all.css' integrity='sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ' crossorigin='anonymous'>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">	
-   <link rel="stylesheet" href="search.css">
-    <link rel="stylesheet" href="stylesheet.css">
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	
-	
-	
-    <title>Travel Experts Database System</title>
-   
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	 
+  
+	<script src="js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script src="https://cdn.staticfile.org/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdn.staticfile.org/popper.js/1.15.0/umd/popper.min.js"></script>
+    <script src="https://cdn.staticfile.org/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <style>
   
           .card, .card-body {
@@ -181,9 +179,9 @@ body{
   
   <!--header for system heardermenu tooltip dropdown-->
     <h3 id="head1">Welcome to Travel Experts Database System (2019)</h3>
-    <img src="icon.jpg" style="width:80px;margin-top:-10px">
+    <img src="icon.png" style="width:100px;margin-top:-20px">
 	<br>
-	<button style="float:right;margin-top:-10px;margin-right:10px" type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
+	<button style="float:right;margin-top:-60px;margin-right:10px" type="button" class="btn btn-success" data-toggle="modal" data-target="#myModal">
       login
    </button>
  
@@ -218,7 +216,7 @@ body{
 
 
 	
-	   <div style="margin-top:-45px;margin-right:78px;display:block" class="container">        		
+	   <div style="margin-top:-60px;margin-right:78px;display:block" class="container">        		
       <!--setting , user can change photos and background image-->   
 		 <a class="nav-item dropdown"><a class="headermenu " class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown"  data-toggle="tooltip" data-placement="bottom" title="setting" >
 	         <i class="fa fa-cog" style="font-size:20px"></i>
@@ -241,6 +239,7 @@ body{
 
 
 <!--navgation bar-->
+<br><br><br><br>
    <nav class="navbar navbar-expand-sm bg-success navbar-dark">
      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
